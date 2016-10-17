27ab2bdb4d161b6dce79d71c3eef9b4284eb8ea1
Maria Viesca <viescamaria3@gmail.com>
2016-10-17 12:41:36 -0500

    Subi los formularios creados el dia de hoy

--- application/views/Home_view.php
+++ application/views/Home_view.php
@@ -8,6 +8,8 @@
 <div align = "center">
 <h1><?=$mensaje?></h1>
 <a href="<?= base_url()?>formularios/css/form.html">Comentar post</a>
+<a href="<?= base_url()?>formularios/css/post.html">Postear</a>
+<a href="<?= base_url()?>formularios/css/control_usuarios.html">Control de usuarios</a>
 </div>
 </body>
 </html>
